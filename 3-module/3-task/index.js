44ee40032d1dbb9a3b4d38c6637f6df20c095386
EgorZhi <vern1992@yandex.ru>
2023-07-13 22:06:29 +0300

    Решение задач занятия 3

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,9 @@
 function camelize(str) {
-  // ваш код...
+  let result = str.split('-');
+  result.forEach((item, index) => {
+    if (index !== 0) {
+      result[index] = item[0].toUpperCase() + item.slice(1);
+    }
+  });
+  return result.join('');
 }
